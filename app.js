ff9ecbad8245365d9bb55544bd498d39ba2cdd23
SaraQian <abigail830@163.com>
2018-10-10 23:25:27 +0800

    init commit

---
+++ app.js
@@ -0,0 +1,138 @@
+//app.js
+let util = require('utils/util.js');
+App({
+  onLaunch: function () {
+    // 展示本地存储能力
+    let logs = wx.getStorageSync('logs') || [];
+    logs.unshift(Date.now());
+    wx.setStorageSync('logs', logs);
+
+    this.bindNetworkChangeRefresh();
+    this.wxLogin();
+  },
+  wxLogin: function () {
+    wx.login({
+      success: res => {
+        console.log(res);
+        wx.setStorageSync('wx_code', res.code);
+        const header = {
+          "X-WX-Code": res.code
+        };
+        console.log("登录中...");
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        wx.request({
+          url: this.globalData.apiBase + "/common/wxLogin",
+          method: "GET",
+          header: header,
+          dataType: "json",
+          complete: res => {
+            wx.hideLoading();
+          },
+          success: (result) => {
+            wx.setStorageSync('skey', result.data.session_key);
+            wx.setStorageSync('openid', result.data.openid);
+            console.log("登录后台成功");
+            util.showSuccess('登录后台成功');
+            console.log(this.globalData);
+            this.globalData.authInfo.skey = result.data.session_key;
+            this.globalData.authInfo.openid = result.data.openid;
+            // 获取用户信息
+            wx.getSetting({
+              success: res2 => {
+                if (res2.authSetting['scope.userInfo']) {
+                  // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+                  wx.getUserInfo({
+                    success: res3 => {
+                      console.log(res3);
+                      if (res3.iv && res3.encryptedData) {
+                        wx.request({
+                          url: this.globalData.apiBase + "/common/decrypt",
+                          method: "GET",
+                          header: {
+                            iv: res3.iv,
+                            encryptedData: res3.encryptedData,
+                            appId: "wxa823794835f994b3",
+                            skey: result.data.session_key
+                          },
+                          dataType: "json",
+                          complete: res => {
+                            wx.hideLoading();
+                          },
+                          success: (result2) => {
+
+                          },
+
+                          fail: (result2) => {
+                            util.showModel('登录后台错误', result2.msg)
+                          },
+                        });
+                      }
+                      // 可以将 res 发送给后台解码出 unionId
+                      this.globalData.userInfo = res3.userInfo;
+
+                      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                      // 所以此处加入 callback 以防止这种情况
+                      if (this.userInfoReadyCallback) {
+                        this.userInfoReadyCallback(res3)
+                      }
+                    }
+                  })
+                }
+              }
+            });
+          },
+
+          fail: (result) => {
+            util.showModel('登录后台错误', result.errMsg)
+          },
+        });
+      }
+    });
+  },
+  bindNetworkChangeRefresh: function () {
+    util.networkTypePromise()
+      .then((isConnected) => this.globalData.isNetworkConnected = isConnected)
+      .catch((isConnected) => this.globalData.isNetworkConnected = isConnected);
+    wx.onNetworkStatusChange(res => {
+      if (res.networkType === 'none') {
+        this.globalData.isNetworkConnected = false;
+        wx.showToast({
+          title: '当前没有网络!',
+          mask: true,
+          icon: "loading",
+          duration: 1000
+        });
+      } else if (res.isConnected) {
+        if (!this.globalData.authInfo.openid) {
+          this.wxLogin();
+        }
+        if (!this.globalData.isNetworkConnected) {
+          let curpage = util.getCurrentPageUrlWithArgs();
+          wx.reLaunch({
+            url: "/" + curpage
+          });
+          this.globalData.isNetworkConnected = true;
+        }
+      } else {// for Android Unknown status
+        this.globalData.isNetworkConnected = false;
+        wx.showToast({
+          title: '网络情况异常!',
+          image: this.globalData.imageBasePath + "/public/error.png",
+          mask: true,
+          duration: 1000
+        });
+      }
+    })
+  },
+  
+  globalData: {
+    authInfo: {
+      skey: null,
+      openid: null
+    },
+    isNetworkConnected: true,
+    userInfo: null,
+    apiBase: "https://wishlist.rabbit-hop.com",
+
+  }
+});
---
+++ utils/util.js
@@ -0,0 +1,143 @@
+const formatTime = date => {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+};
+
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+};
+
+const range = (start, end) => new Array(end - start + 1).fill(start).map((el, i) => start + i);
+
+const pad = (n, width, z) => {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+};
+
+const showSuccess = text => wx.showToast({
+  title: text,
+  icon: 'success'
+});
+
+const showModel = (title, content) => {
+  wx.hideToast();
+
+  wx.showModal({
+    title,
+    content: JSON.stringify(content),
+    showCancel: false
+  })
+};
+
+const shorten = (content, length) => {
+  return content.length > length ? content.substr(0, length) + "..." : content;
+};
+
+const showLoading = text => wx.showToast({
+  title: text,
+  icon: 'loading',
+  duration: 1000
+});
+
+const parameterMap = {
+  gender: [{ id: 1, key: "male", name: '男' }, { id: 2, key: "female", name: '女' }],
+  treatmentMethod: [
+    { id: 1, key: "medication", checked: false, name: '药物治疗' },
+    { id: 2, key: "hemodialysis", checked: false, name: '血液透析治疗' },
+    { id: 3, key: "peritoneal-dialysis", checked: false, name: '腹膜透析治疗' },
+    { id: 4, key: "transplantation", checked: false, name: '肾脏移植' }
+  ],
+  sportRate: [
+    { id: 1, key: "light", name: '轻度（如：长期坐办公室）' },
+    { id: 2, key: "medium", name: '中度（如：不时外出跑业务）' },
+    { id: 3, key: "severe", name: '重度（如：搬运）' }
+  ],
+  otherDisease: [//高血压/高血脂（甘油三酯/胆固醇/both）/高血糖/高尿酸/无
+    { id: 1, key: "hypertension", checked: false, name: '高血压' },
+    { id: 2, key: "triglyceride", checked: false, name: '高甘油三酯' },
+    { id: 3, key: "cholesterol", checked: false, name: '高胆固醇' },
+    { id: 4, key: "hyperglycemia", checked: false, name: '高血糖' },
+    { id: 5, key: "hyperuricacidemia", checked: false, name: '高尿酸' }
+  ],
+  irritability: [//奶/蛋/贝壳/虾蟹鱼/面粉/坚果/黄豆/玉米
+    { id: 1, key: "milk", checked: false, name: '奶' },
+    { id: 2, key: "egg", checked: false, name: '蛋' },
+    { id: 3, key: "crostacei", checked: false, name: '贝壳' },
+    { id: 4, key: "fish-prawn-crab", checked: false, name: '鱼虾蟹' },
+    { id: 5, key: "flour", checked: false, name: '面粉' },
+    { id: 6, key: "nuts", checked: false, name: '坚果' },
+    { id: 7, key: "soya", checked: false, name: '黄豆' },
+    { id: 8, key: "corn", checked: false, name: '玉米' }
+  ],
+};
+
+const getAliasSingleOption = (options, value) => parameterMap[options].filter(item => item.key === value)[0].name;
+
+const getAliasMultiOption = (options, value) => parameterMap[options].filter(item => value.includes(item)).map(item => item.name).join(",");
+
+const cutMessage = (source, length) => source.length > length ? source.substr(0, length) + "..." : source;
+
+const getCurrentPageUrl = () => {
+  let pages = getCurrentPages();
+  let currentPage = pages[pages.length - 1];
+  return currentPage.route;
+};
+
+const getCurrentPageUrlWithArgs = () => {
+  let pages = getCurrentPages();
+  let currentPage = pages[pages.length - 1];
+  let url = currentPage.route;
+  let options = currentPage.options;
+
+  let urlWithArgs = url + '?';
+  for (let key in options) {
+    let value = options[key];
+    urlWithArgs += key + '=' + value + '&';
+  }
+  urlWithArgs = urlWithArgs.substring(0, urlWithArgs.length - 1);
+
+  return urlWithArgs
+};
+
+const networkTypePromise = () => new Promise((resolve, reject) => {
+  let req = wx.getNetworkType({
+    success: function (res) {
+      var networkType = res.networkType;
+      if (networkType === 'none') {
+        resolve(false)
+      } else {
+        resolve(true)
+      }
+    },
+    fail() {
+      reject(false)
+    }
+  });
+  console.log(req);
+});
+
+module.exports = {
+  formatTime: formatTime,
+  formatNumber: formatNumber,
+  range: range,
+  pad: pad,
+  shorten: shorten,
+  showSuccess: showSuccess,
+  showModel: showModel,
+  showLoading: showLoading,
+  parameterMap: parameterMap,
+  getAliasSingleOption: getAliasSingleOption,
+  cutMessage: cutMessage,
+  getAliasMultiOption: getAliasMultiOption,
+  getCurrentPageUrl: getCurrentPageUrl,
+  getCurrentPageUrlWithArgs: getCurrentPageUrlWithArgs,
+  networkTypePromise: networkTypePromise,
+};
