a96ba9fe1a2e0037d856ef8588f6f4272cf13e2b
bryan-cheung <bryan.l.cheung@gmail.com>
2018-12-02 18:53:17 -0500

    explicitly login wx in main page

--- pages/index_after_authorize/index_after.js
+++ pages/index_after_authorize/index_after.js
@@ -39,6 +39,10 @@ Page({
   * 生命周期函数--监听页面显示
   */
   onShow: function () {
+    if (app.globalData.authInfo.openid == null) {
+      console.log("openID is null - try to login wx again");
+      app.wxLogin();
+    }
     this.initWishLists();
     // this.setWishListData();
   },
